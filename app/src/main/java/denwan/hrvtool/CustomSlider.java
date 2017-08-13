8b9537239f81fd247124ce9cee7d42d807eccbd6
Dennis Wandschura <dennis.wandschura@gmail.com>
2017-08-13 20:49:17 +0200

    added middle text to CustomSlider

--- app/src/main/java/denwan/hrvtool/CustomSlider.java
+++ app/src/main/java/denwan/hrvtool/CustomSlider.java
@@ -13,9 +13,9 @@ import android.widget.TextView;
  */
 
 public class CustomSlider extends RelativeLayout {
-    RelativeLayout m_layout;
     TextView m_textViewHeader;
     TextView m_textViewMinText;
+    TextView m_textViewMidText;
     TextView m_textViewMaxText;
     SeekBar m_seekbar;
 
@@ -25,20 +25,23 @@ public class CustomSlider extends RelativeLayout {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CustomSlider);
 
         String minText = a.getString(R.styleable.CustomSlider_minText);
+        String midText = a.getString(R.styleable.CustomSlider_midText);
         String maxText = a.getString(R.styleable.CustomSlider_maxText);
         String headerText = a.getString(R.styleable.CustomSlider_headerText);
         int maxValue = a.getInteger(R.styleable.CustomSlider_maxValue, 10);
 
         String service = Context.LAYOUT_INFLATER_SERVICE;
         LayoutInflater li = (LayoutInflater) getContext().getSystemService(service);
-        m_layout = (RelativeLayout) li.inflate(R.layout.customslider, this, true);
+        RelativeLayout layout = (RelativeLayout) li.inflate(R.layout.customslider, this, true);
 
-        m_textViewMinText = (TextView) m_layout.findViewById(R.id.textViewMinText);
-        m_textViewMaxText = (TextView) m_layout.findViewById(R.id.textViewMaxText);
-        m_textViewHeader = (TextView) m_layout.findViewById(R.id.textViewHeader);
-        m_seekbar = (SeekBar) m_layout.findViewById(R.id.customSliderSeekBar);
+        m_textViewMinText = (TextView) layout.findViewById(R.id.textViewMinText);
+        m_textViewMidText = (TextView) layout.findViewById(R.id.textViewMidText);
+        m_textViewMaxText = (TextView) layout.findViewById(R.id.textViewMaxText);
+        m_textViewHeader = (TextView) layout.findViewById(R.id.textViewHeader);
+        m_seekbar = (SeekBar) layout.findViewById(R.id.customSliderSeekBar);
 
         m_textViewMinText.setText(minText);
+        m_textViewMidText.setText(midText);
         m_textViewMaxText.setText(maxText);
         m_textViewHeader.setText(headerText);
 
